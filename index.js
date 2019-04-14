84a82f404640e6810724b0bbb7bd84ea11e5b72e
Kevin Mannix <kmannix@bu.edu>
2019-04-14 16:48:29 -0400

    add Account methods

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const Account = require('./lib/account')
 const Client = require('./lib/client')
 const Games = require('./lib/games')
 
 class Lichess {
   constructor(token = null) {
     const client = new Client(token)
+    this._account = new Account(client)
     this._games = new Games(client)
+  }
+
+  get account() {
+    return this._account
   }
 
   get games() {
