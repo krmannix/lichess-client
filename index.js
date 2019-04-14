db3fa27c534bd00d3d4104e858694f5e30ea5364
Kevin Mannix <kmannix@bu.edu>
2019-04-14 16:00:41 -0400

    lib/{base -> client}.js, move functionality of the previous lib/client.js to index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
 const Client = require('./lib/client')
+const Games = require('./lib/games')
 
-module.exports = Client
+class Lichess {
+  constructor(token = null) {
+    const client = new Client(token)
+    this._games = new Games(client)
+  }
+
+  get games() {
+    return this._games
+  }
+}
+
+module.exports = Lichess
